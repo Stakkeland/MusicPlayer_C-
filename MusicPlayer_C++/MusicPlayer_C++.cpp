2a9f130473f94737a698dc5d9c44cda7d256ce8a
Stakkeland <sawyerstakkeland@gmail.com>
2024-05-03 10:36:26 -0700

    All parts wanted are done

--- MusicPlayer_C++/MusicPlayer_C++.cpp
+++ MusicPlayer_C++/MusicPlayer_C++.cpp
@@ -11,29 +11,30 @@ int main();
 int playSound(const wchar_t* filename);
 
 int main() {
-    cout << "Welcome\n";
     // Play a WAV file
+    cout << "Welcome \n";
 
+    // Variable declaration
     int option;
-    cout << "Choose a song \n 0:End \n 1:Epic \n 2:Thrill\n";
-    cin >> option;
-    cout << "Playing Music \n";
-
     bool goOn = FALSE;
     const wchar_t* filename = nullptr;
+
     while (goOn == FALSE)
     {
+        cout << "Choose a song \n 0:End \n 1:Epic \n 2:Thrill \n : ";
+        cin >> option;
+
         if (option == 1)
         {
             filename = L"HOTD_Song.wav";
-            goOn = TRUE;
+            goOn = FALSE;
 
             playSound(filename);
         }
         else if (option == 2)
         {
             filename = L"Hardpoints_Song.wav";
-            goOn = TRUE;
+            goOn = FALSE;
 
             playSound(filename);
         }
@@ -44,31 +45,33 @@ int main() {
         }
         else
         {
-            cout << "Please choose a number that is available";
+            cout << "Please choose a number that is available \n";
             goOn = FALSE;
         }
 
     };
     
-
     return 0;
 }
 
+/********************************************************************
+*playSound
+*This function plays and stops the song given by the user
+*Press enter to start and stop music
+*input = Enter 
+*
+**********************************************************************/
 int playSound(const wchar_t* filename) {
 
-    PlaySound(filename, NULL, SND_FILENAME | SND_ASYNC | SND_LOOP);
-
     string input;
-    getline(cin, input);
-    PlaySound(0, 0, 0);
-    cout << "Stopped music \n";
 
-    getline(cin, input);
-    PlaySound(0, 0, 0);
-    cout << "Playing music \n";
+    cout << "Music playing \nPress enter to stop \n";
     PlaySound(filename, NULL, SND_FILENAME | SND_ASYNC | SND_LOOP);
 
     getline(cin, input);
+    PlaySound(filename, NULL, SND_FILENAME | SND_ASYNC | SND_LOOP);
+    getline(cin, input);
+    PlaySound(0, 0, 0);
 
     cout << "Music Finished \n";
 
